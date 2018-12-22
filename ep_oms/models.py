fbb97665ed813011a9cf6cd7afacbc4df99289a4
taehyunlim <taehyun.lim@gmail.com>
2018-12-21 17:59:09 -0800

    Adds Admin table to model

--- ep_oms/models.py
+++ ep_oms/models.py
@@ -1,5 +1,22 @@
 from ep_oms import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Admin(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  email = db.Column(db.String(120), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+
+  def __repr__(self):
+    return '<Admin {}>'.format(self.username)
+
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password(self.password_hash, password)
+
 
 class Customer(db.Model):
   id = db.Column(db.Integer, primary_key=True)
