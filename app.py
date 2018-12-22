69320b926acd2dede8580998d22a0cc12356c4dd
taehyunlim <taehyun.lim@gmail.com>
2018-12-22 09:20:25 -0800

    Login feature for admin

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from ep_oms import application, db
-from ep_oms.models import Address, Product, LineItem, Order, Parcel, Shipment, Carrier, Customer
+from ep_oms.models import Address, Product, LineItem, Order, Parcel, Shipment, Carrier, Customer, Admin
 
 @application.shell_context_processor
 def make_shell_context():
   return { 'db': db, 'Address': Address, 'Product': Product, 'LineItem': LineItem, \
     'Order': Order, 'Parcel': Parcel, 'Shipment': Shipment, 'Carrier': Carrier, \
-    'Customer': Customer}
+    'Customer': Customer, 'Admin': Admin}
