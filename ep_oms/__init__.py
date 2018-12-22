338f932c1018fed4b1adb1670ed9002126ace216
taehyunlim <taehyun.lim@gmail.com>
2018-12-22 09:20:05 -0800

    Login feature for admin

--- ep_oms/__init__.py
+++ ep_oms/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 application = Flask(__name__)
 application.config.from_object(Config)
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
+login = LoginManager(application)
+login.logn_view = 'login'
 
 from ep_oms import routes, models
