2cbcaa86ab1b0f511e32451ee1aa04b83e6e9f88
taehyunlim <taehyun.lim@gmail.com>
2018-12-22 09:21:39 -0800

    Login feature for admin

--- ep_oms/models.py
+++ ep_oms/models.py
@@ -1,8 +1,9 @@
-from ep_oms import db
+from ep_oms import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class Admin(db.Model):
+class Admin(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
@@ -17,6 +18,9 @@ class Admin(db.Model):
   def check_password(self, password):
     return check_password(self.password_hash, password)
 
+@login.user_loader
+def load_user(id):
+  return Admin.query.get(int(id))
 
 class Customer(db.Model):
   id = db.Column(db.Integer, primary_key=True)
