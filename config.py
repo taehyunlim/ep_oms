f90d1195bcd413184791fb557176a22daf51e78f
taehyunlim <taehyun.lim@gmail.com>
2019-01-24 13:16:22 -0800

    EP API Key

--- config.py
+++ config.py
@@ -16,4 +16,5 @@ class Config(object):
   MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-  ADMINS = ['taehyun.lim@gmail.com']
+  ADMINS = ['tae@easypost.com']
+  EP_API_KEY = os.environ.get('EP_API_KEY')
